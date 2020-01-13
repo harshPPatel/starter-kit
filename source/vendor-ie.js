1d0991b4e2d71c37cb0f2c2fa2ae2311abfae8e4
Harsh Patel <harshppatel2880@gmail.com>
2020-01-13 08:58:10 -0600

    Cleared the repo

--- source/vendor-ie.js
+++
@@ -1,7 +0,0 @@
-// Import vendor files dedicated to older browsers like Internet Explorer
-
-// html5shiv to add HTML5 tags support
-import 'html5shiv';
-
-// selectivizr to add support for CSS3 selectors
-import 'selectivizr';
